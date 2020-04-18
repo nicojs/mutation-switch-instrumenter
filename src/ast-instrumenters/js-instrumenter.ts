3cc160a7c2a2c26fd6cbcc5da64a6f227b9d38cf
Nico Jansen <jansennico@gmail.com>
2020-04-18 09:59:03 +0200

    Don't mutate decorators, Angular compiler doesn't like that one bit

--- src/ast-instrumenters/js-instrumenter.ts
+++ src/ast-instrumenters/js-instrumenter.ts
@@ -5,13 +5,13 @@ import { mutate } from '../mutators';
 import { declareGlobal, isTypeAnnotation, isImportDeclaration } from '../util/syntax-helpers';
 import { AstFormat } from '../syntax';
 
-export const instrumentJS: AstInstrumenter<AstFormat.JS> = (
+export const instrumentJS: AstInstrumenter<AstFormat.JS | AstFormat.TS> = (
   { root, originFile },
   mutantCollector
 ) => {
   traverse(root, {
     enter(path) {
-      if (isTypeAnnotation(path) || isImportDeclaration(path)) {
+      if (isTypeAnnotation(path) || isImportDeclaration(path) || path.isDecorator()) {
         // Don't mutate type declarations or import statements
         path.skip();
       } else {
